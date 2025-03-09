4e9c219a5ea0db82969dabb317cb235478a02816
Juan M. Cortéz <juanm.cortez@gmail.com>
2025-03-09 17:20:47 -0300

    📦️ Tailwindcss + Alpinejs

---
+++ resources/js/laracare.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025
+ *
+ *  @author Juan Manuel Cortéz <juanm.cortez@gmail.com>
+ *  @copyright 2025 Nobidium LLC.
+ *  @license MIT License
+ */
+
+import './bootstrap';
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+Alpine.start()
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
+/*
+ * Copyright (c) 2025
+ *
+ *  @author Juan Manuel Cortéz <juanm.cortez@gmail.com>
+ *  @copyright 2025 Nobidium LLC.
+ *  @license MIT License
+ */
+
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/laracare.css', 'resources/js/laracare.js'],
             refresh: true,
         }),
         tailwindcss(),
