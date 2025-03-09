135f0ce971e8ff21688e0620ad10dc6320e5cab2
Juan M. Cortéz <juanm.cortez@gmail.com>
2025-03-09 18:09:53 -0300

    🚧 Protected routes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('landing');
+Route::middleware('auth')->group(function () {
+    // Landing now is protected with login
+    Route::get('/', function () {
+        return view('landing');
+    });
 });
+
