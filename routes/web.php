025da9fdac684b7615699e1dc2b4a7169ea05dd6
Juan M. Cortéz <juanm.cortez@gmail.com>
2025-03-09 17:21:41 -0300

    🚧 New landing page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
+/*
+ * Copyright (c) 2025
+ *
+ *  @author Juan Manuel Cortéz <juanm.cortez@gmail.com>
+ *  @copyright 2025 Nobidium LLC.
+ *  @license MIT License
+ */
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
