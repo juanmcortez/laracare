e79e0ac69ae1519ac68244f83874b9c2ede61da5
Juan M. Cortéz <juanm.cortez@gmail.com>
2025-03-13 17:26:52 -0300

    📦️ Flaticon attribute

---
+++ resources/views/components/ui/general/flaticon.blade.php
@@ -0,0 +1,11 @@
+<a href="https://www.flaticon.com/uicons" target="_blank" class="inline-flex flex-row justify-center items-center">
+    {{ __("Uicons by") }}
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1 fill-primary"
+         title="{{ __("Uicons by Flaticon") }}">
+        <g transform="matrix(0.48326169,0,0,0.4832676,-2.9052809e-4,1.9054741e-5)">
+            <path
+                d="M 13.462,7.626 H 7.816 L 6.05,4.606 h 9.178 L 17.902,0 H 2.732 a 2.73,2.73 0 0 0 -2.36,1.354 2.652,2.652 0 0 0 0,2.707 l 9.673,16.606 c 0.76,1.288 2.443,1.717 3.747,0.958 a 2.86,2.86 0 0 0 0.974,-0.958 l 0.313,-0.545 -4.44,-7.659 z"/>
+            <path d="M 24.473,1.354 A 2.71,2.71 0 0 0 22.112,0 h -0.66 l -7.264,12.463 2.675,4.606 7.61,-13.041 a 2.67,2.67 0 0 0 0,-2.674 z"/>
+        </g>
+    </svg>
+</a>
