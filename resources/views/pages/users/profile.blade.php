5086723a57300b807778789638a10ce5ad322a3e
Juan M. Cortéz <juanm.cortez@gmail.com>
2025-03-18 08:59:07 -0300

    🚧 User pages

--- resources/views/pages/users/profile.blade.php
+++ resources/views/pages/users/profile.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.main>
     <x-slot:title>{{ __('Your profile details :Name', ['name' => Auth::user()->username]) }}</x-slot:title>
     <x-slot:section>{{ __('User profile') }}</x-slot:section>
-    <x-slot:sidebar></x-slot:sidebar>
+    <x-slot:sidebar>@include('pages.users.submenu')</x-slot:sidebar>
     {{ $user }}
 </x-layouts.main>
