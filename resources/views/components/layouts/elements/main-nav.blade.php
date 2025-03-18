d2bf941df94ba8d8f16ca5959ae55ee4c2b3616a
Juan M. Cortéz <juanm.cortez@gmail.com>
2025-03-18 08:59:46 -0300

    🚧 Main layout changes

--- resources/views/components/layouts/elements/main-nav.blade.php
+++ resources/views/components/layouts/elements/main-nav.blade.php
@@ -2,13 +2,20 @@
     <div class="logo"><i class="fi fi-rs-webhook"></i></div>
     <nav class="navigation">
         <ul>
-            <li>AA</li>
+            <li>
+                <x-ui.general.linkwicon icon="fi-rs-dashboard-monitor" :url="route('main')" :title="__('Dashboard')"
+                                        :class="Request::routeIs('main') ? 'active' : null"/>
+            </li>
+            <li>
+                <x-ui.general.linkwicon icon="fi-rs-member-list" :url="route('users.list')" :title="__('User')"
+                                        :class="Request::routeIs('users.*') ? 'active' : null"/>
+            </li>
         </ul>
-        {{-- <ul>
-            <li>BB</li>
-        </ul> --}}
         <ul>
-            <li>CC</li>
+            <li>
+                <x-ui.general.linkwicon icon="fi-rs-admin-alt" :url="route('settings')" :title="__('Settings')"
+                                        :class="Request::routeIs('settings') ? 'active' : null"/>
+            </li>
         </ul>
     </nav>
 </aside>
