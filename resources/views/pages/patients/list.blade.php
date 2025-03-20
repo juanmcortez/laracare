69e70f16193a7f647d7081fdb9c8963962a6ba0f
Juan M. Cortéz <juanm.cortez@gmail.com>
2025-03-20 10:15:26 -0300

    💄 Patient UI improvements

--- resources/views/pages/patients/list.blade.php
+++ resources/views/pages/patients/list.blade.php
@@ -4,7 +4,8 @@
     <x-slot:section>{{ __('Patients') }}</x-slot:section>
     <x-slot:sidebar>@include('pages.patients.submenu')</x-slot:sidebar>
     @foreach($patients as $patient)
-        <div class="flex flex-row mb-2 text-center">
+        <div class="flex flex-row mb-2 text-center hover:cursor-pointer" x-data
+             @click="window.location.href = '{{ route('patients.profile', ['pid' => $patient->pid]) }}'">
             <div class="w-4/12 text-left">{{ \Str::ucfirst($patient->demographic->title) . '. ' . $patient->demographic->full_name }}</div>
             <div class="w-2/12">{{ $patient->demographic->date_of_birth }}</div>
             <div class="w-2/12">{{ \Str::ucfirst($patient->demographic->gender) }}</div>
